ed8f74c8c2f43bf4a8e5b8ed2b06192e7c8f684d
hitesh409 <98460671+hitesh409@users.noreply.github.com>
2023-03-17 18:57:31 +0530

    Initial commit

---
+++ dist/js/index.js
@@ -0,0 +1,14 @@
+const course  = ['cpp','java','python','jamstack'];
+let randomNum = Math.floor(Math.random()*course.length);
+document.querySelector('.course').append(course[randomNum]);
+fetch('https://api.imgflip.com/get_memes')
+    .then (response => response.json())
+    .then (data =>{
+        console.log(data.data.memes)
+        for (var i=0;i<data.data.memes.length; i++) {
+            var img = documeudemy
+            document.createElement("img");
+            img.src = data.data.memes [i].url;
+            document.getElementById('img-container').appendChild(img);
+        }
+})
