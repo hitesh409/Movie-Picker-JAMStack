34802f238c80c3694e6728de118c12d5e8b2492f
hitesh409 <98460671+hitesh409@users.noreply.github.com>
2023-03-19 11:43:57 +0530

    Complete movie app for project

--- src/_data/data.js
+++ src/_data/data.js
@@ -1,15 +1,58 @@
 const axios = require('axios');
 module.exports = async function() //whatever this results in will be exported
 {
+
+    let movieData = {
+        actionMovie: [],
+        adventureMovie: [],
+        comedyMovie: []
+    }
+
     try{
-        const response = await axios.get('https://api.imgflip.com/get_memes');
-        return response.data.data;
+        const actionMovie = await axios.get(' https://api.themoviedb.org/3/discover/movie?api_key=6b833c4c7c8f05d91cf489269b4c685c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28&with_watch_monetization_types=flatrate');
+        for(let i=0;i<actionMovie.data.results.length;i++)
+        {
+            let tempMovieObj = {
+                title: actionMovie.data.results[i].title,
+                poster_path: actionMovie.data.results[i].poster_path
+            }
+            movieData.actionMovie.push(tempMovieObj);
+        }
+        
+        
+        const adventureMovie = await axios.get(' https://api.themoviedb.org/3/discover/movie?api_key=6b833c4c7c8f05d91cf489269b4c685c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=12&with_watch_monetization_types=flatrate');
+        for(let i=0;i<adventureMovie.data.results.length;i++)
+        {
+            let tempMovieObj = {
+                title: adventureMovie.data.results[i].title,
+                poster_path: adventureMovie.data.results[i].poster_path
+            }
+            movieData.adventureMovie.push(tempMovieObj);
+        }
+        
+        
+        const comedyMovie = await axios.get(' https://api.themoviedb.org/3/discover/movie?api_key=6b833c4c7c8f05d91cf489269b4c685c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=35&with_watch_monetization_types=flatrate');
+        for(let i=0;i<comedyMovie.data.results.length;i++)
+        {
+            let tempMovieObj = {
+                title: comedyMovie.data.results[i].title,
+                poster_path: comedyMovie.data.results[i].poster_path
+            }
+            movieData.comedyMovie.push(tempMovieObj);
+        }
+        console.log(movieData);
+        
+        
+        
+        return movieData;
     }
     catch(error)
     {
         console.log(error);
     }
 }
+
+
 
 // {
 //     "memes": [
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,7 @@
-const course  = ['cpp','java','python','jamstack'];
-let randomNum = Math.floor(Math.random()*course.length);
-document.querySelector('.course').append(course[randomNum]);
-// fetch('https://api.imgflip.com/get_memes')
-//     .then (response => response.json())
-//     .then (data =>{
-//         console.log(data.data.memes)
-//         for (var i=0;i<data.data.memes.length; i++) {
-//             var img = document.createElement('img');
-//             document.createElement("img");
-//             img.src = data.data.memes [i].url;
-//             document.getElementById('img-container').appendChild(img);
-//         }
-// })
+const genres = ['action','adventure','comedy'];
+
+let randomNum = Math.floor(Math.random()*genres.length);
+let randomIdx = Math.floor(Math.random()*20);
+
+const genre = genres[randomNum];
+document.getElementById(genre + randomIdx).className = "show";
